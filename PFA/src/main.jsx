fb453f6bda2e6248f07cd27672f768368f4ef96b
dienabadhb <d_diakhaby@etu-webschoolfactory.fr>
2024-05-16 14:58:02 +0200

    TKT

--- PFA/src/main.jsx
+++ PFA/src/main.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+import ConnexionP from '../Plaquiste/ConnexionP.jsx';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <div></div>,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ConnexionP></ConnexionP> 
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
