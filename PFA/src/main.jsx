2c26635e079b7ec36e37ab79af61e923c86ae2cc
Roukik <148196386+Roukik@users.noreply.github.com>
2024-05-17 02:55:45 +0200

    .

--- PFA/src/main.jsx
+++ PFA/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import {createBrowserRouter, RouterProvider,} from "react-router-dom";
-import ConnexionP from "../Plaquiste/pagesPlaquiste/ConnexionP"
+import ConnexionP from "../PagesCommunes/ConnexionP"
 
 
 
