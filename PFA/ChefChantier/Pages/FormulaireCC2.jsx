f6f6678331ea1b4ceb7527851503d3700e653ed9
Roukik <148196386+Roukik@users.noreply.github.com>
2024-05-28 01:35:59 +0200

    +

--- PFA/ChefChantier/Pages/FormulaireCC2.jsx
+++ PFA/ChefChantier/Pages/FormulaireCC2.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
-import { 
-  Typography, 
-  Button, 
-  TextField, 
-  Grid, 
-  Box, 
-  Card, 
-  CardContent, 
-  IconButton, 
-  Select, 
-  MenuItem, 
-  InputLabel, 
-  FormControl 
+import {
+  Typography,
+  Button,
+  TextField,
+  Grid,
+  Box,
+  Card,
+  CardContent,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  Stepper,
+  Step,
+  StepLabel,
+  StepContent,
+  Paper
 } from '@mui/material';
 import { styled } from '@mui/system';
 
@@ -28,6 +32,21 @@ const StyledCard = styled(Card)({
   marginBottom: 70,
 });
 
+const steps = [
+  {
+    label: 'Définissez les détails de la pièce',
+    description: 'Saisissez les informations sur le type de pièce, le nombre de pièces, etc.',
+  },
+  {
+    label: 'Définissez les murs et les ouvertures',
+    description: 'Ajoutez des murs, des fenêtres, des portes, et définissez leurs caractéristiques.',
+  },
+  {
+    label: 'Téléchargez des images',
+    description: 'Téléchargez des images de la pièce et ajoutez des liens URL si nécessaire.',
+  },
+];
+
 const FormulaireCC2 = () => {
   const [rooms, setRooms] = useState([{
     type: '',
@@ -41,6 +60,7 @@ const FormulaireCC2 = () => {
     heating: '',
     images: []
   }]);
+  const [activeStep, setActiveStep] = useState(0);
 
   const handleAddRoom = () => {
     setRooms([...rooms, {
@@ -81,181 +101,218 @@ const FormulaireCC2 = () => {
     setRooms(newRooms);
   };
 
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
   return (
     <Content>
       <Typography variant="h4">Commencez l’état des lieux !</Typography>
-      {rooms.map((room, index) => (
-        <StyledCard key={index}>
-          <CardContent>
-            <WelcomeText variant="h6">Step {index + 1}</WelcomeText>
-            <Typography variant="body1">Premier type de pièce(s)</Typography>
-            <FormControl fullWidth margin="normal">
-              <InputLabel>Type de pièce</InputLabel>
-              <Select
-                name="type"
-                value={room.type}
-                onChange={(e) => handleInputChange(index, e)}
-              >
-                <MenuItem value="cuisine">Cuisine</MenuItem>
-                <MenuItem value="toilettes">Toilettes</MenuItem>
-                <MenuItem value="pièce simple">Pièce simple</MenuItem>
-                <MenuItem value="escaliers">Escaliers</MenuItem>
-                <MenuItem value="salle de bain">Salle de bain</MenuItem>
-                <MenuItem value="ascenseur">Ascenseur</MenuItem>
-              </Select>
-            </FormControl>
-            <TextField 
-              fullWidth 
-              label="Nombre de pièces" 
-              margin="normal" 
-              type="number" 
-              name="pieces" 
-              value={room.pieces} 
-              onChange={(e) => handleInputChange(index, e)} 
-            />
-            <FormControl fullWidth margin="normal">
-              <InputLabel>Antécédents des pièces</InputLabel>
-              <Select
-                name="elements"
-                value={room.elements}
-                onChange={(e) => handleInputChange(index, e)}
-              >
-                <MenuItem value="aucun">Aucun</MenuItem>
-                <MenuItem value="neuf">Neuf</MenuItem>
-                <MenuItem value="ancien">Ancien</MenuItem>
-              </Select>
-            </FormControl>
-            <Typography variant="body1" style={{ marginTop: 20 }}>
-              Définissez le type et la longueur de chaque mur
-            </Typography>
-            {room.walls.map((wall, wallIndex) => (
-              <Grid container spacing={2} key={wallIndex}>
-                <Grid item xs={6}>
-                  <FormControl fullWidth margin="normal">
-                    <InputLabel>Type</InputLabel>
-                    <Select
-                      name="type"
-                      value={wall.type}
-                      onChange={(e) => handleWallChange(index, wallIndex, e)}
-                    >
-                      <MenuItem value="type1">Type 1</MenuItem>
-                      <MenuItem value="type2">Type 2</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField 
-                    fullWidth 
-                    label="Longueur (cm)" 
-                    margin="normal" 
-                    type="number" 
-                    name="length" 
-                    value={wall.length} 
-                    onChange={(e) => handleWallChange(index, wallIndex, e)} 
-                  />
-                </Grid>
-              </Grid>
-            ))}
-            <Button variant="outlined" color="primary" onClick={() => handleAddWall(index)}>
-              Ajouter un mur
-            </Button>
-            <TextField 
-              fullWidth 
-              label="Épaisseur du mur (cm)" 
-              margin="normal" 
-              type="number" 
-              name="thickness" 
-              value={room.thickness} 
-              onChange={(e) => handleInputChange(index, e)} 
-            />
-            <TextField 
-              fullWidth 
-              label="Nombre de fenêtres" 
-              margin="normal" 
-              type="number" 
-              name="windows" 
-              value={room.windows} 
-              onChange={(e) => handleInputChange(index, e)} 
-            />
-            <TextField 
-              fullWidth 
-              label="Nombre de portes" 
-              margin="normal" 
-              type="number" 
-              name="doors" 
-              value={room.doors} 
-              onChange={(e) => handleInputChange(index, e)} 
-            />
-            <FormControl fullWidth margin="normal">
-              <InputLabel>Type de plafond</InputLabel>
-              <Select
-                name="flooring"
-                value={room.flooring}
-                onChange={(e) => handleInputChange(index, e)}
-              >
-                <MenuItem value="sous plafond">Sous plafond</MenuItem>
-                <MenuItem value="plafond suspendu">Plafond suspendu</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl fullWidth margin="normal">
-              <InputLabel>Type d'isolation</InputLabel>
-              <Select
-                name="heating"
-                value={room.heating}
-                onChange={(e) => handleInputChange(index, e)}
-              >
-                <MenuItem value="extérieur">Extérieur</MenuItem>
-                <MenuItem value="intérieur">Intérieur</MenuItem>
-              </Select>
-            </FormControl>
-            <Typography variant="body1" style={{ marginTop: 20 }}>
-              Téléchargez des images
-            </Typography>
-            <Button variant="outlined" component="label"
-            fullWidth style={{ marginTop: 10 }}>
-            Faites glisser vos fichiers ou parcourez les
-            <input 
-              type="file" 
-              hidden 
-              multiple 
-              onChange={(e) => handleFileChange(index, e)} 
-            />
-            </Button>
-            <Typography variant="body2" style={{ marginTop: 10 }}>
-            Ne prend en charge que les fichiers .jpg, .png, .svg et les fichiers zip
-            </Typography>
-            <Typography variant="body2" style={{ marginTop: 10 }}>
-            ou
-            </Typography>
-            <TextField 
-            fullWidth 
-            label="Ajouter un lien URL" 
-            margin="normal" 
-            type="url" 
-            />
-            <Button variant="contained" color="secondary" style={{ marginTop: 10 }}>
-            Télécharger
-            </Button>
-            </CardContent>
-            </StyledCard>
-            ))}
-            <Box mt={2}>
-            <Button variant="contained" color="primary" onClick={handleAddRoom}>
-            Ajouter une pièce
-            </Button>
-            </Box>
-            <Box mt={2}>
-            <Button variant="contained" color="primary">
-            Enregistrer
-            </Button>
-            </Box>
-            <Box mt={2}>
-            <Button variant="contained" color="secondary">
-            Télécharger en .XLS
-            </Button>
-            </Box>
-            </Content>
-            );
-            };
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((step, index) => (
+          <Step key={step.label}>
+            <StepLabel>{step.label}</StepLabel>
+            <StepContent>
+              <Typography>{step.description}</Typography>
+              {rooms.map((room, roomIndex) => (
+                <StyledCard key={roomIndex}>
+                  <CardContent>
+                    <WelcomeText variant="h6">Step {roomIndex + 1}</WelcomeText>
+                    {index === 0 && (
+                      <>
+                        <FormControl fullWidth margin="normal">
+                          <InputLabel>Type de pièce</InputLabel>
+                          <Select
+                            name="type"
+                            value={room.type}
+                            onChange={(e) => handleInputChange(roomIndex, e)}
+                          >
+                            <MenuItem value="cuisine">Cuisine</MenuItem>
+                            <MenuItem value="toilettes">Toilettes</MenuItem>
+                            <MenuItem value="pièce simple">Pièce simple</MenuItem>
+                            <MenuItem value="escaliers">Escaliers</MenuItem>
+                            <MenuItem value="salle de bain">Salle de bain</MenuItem>
+                            <MenuItem value="ascenseur">Ascenseur</MenuItem>
+                          </Select>
+                        </FormControl>
+                        <TextField 
+                          fullWidth 
+                          label="Nombre de pièces" 
+                          margin="normal" 
+                          type="number" 
+                          name="pieces" 
+                          value={room.pieces} 
+                          onChange={(e) => handleInputChange(roomIndex, e)} 
+                        />
+                        <FormControl fullWidth margin="normal">
+                          <InputLabel>Antécédents des pièces</InputLabel>
+                          <Select
+                            name="elements"
+                            value={room.elements}
+                            onChange={(e) => handleInputChange(roomIndex, e)}
+                          >
+                            <MenuItem value="aucun">Aucun</MenuItem>
+                            <MenuItem value="neuf">Neuf</MenuItem>
+                            <MenuItem value="ancien">Ancien</MenuItem>
+                          </Select>
+                        </FormControl>
+                      </>
+                    )}
+                    {index === 1 && (
+                      <>
+                        <Typography variant="body1" style={{ marginTop: 20 }}>
+                          Définissez le type et la longueur de chaque mur
+                        </Typography>
+                        {room.walls.map((wall, wallIndex) => (
+                          <Grid container spacing={2} key={wallIndex}>
+                            <Grid item xs={6}>
+                              <FormControl fullWidth margin="normal">
+                                <InputLabel>Type</InputLabel>
+                                <Select
+                                  name="type"
+                                  value={wall.type}
+                                  onChange={(e) => handleWallChange(roomIndex, wallIndex, e)}
+                                >
+                                  <MenuItem value="type1">Type 1</MenuItem>
+                                  <MenuItem value="type2">Type 2</MenuItem>
+                                </Select>
+                              </FormControl>
+                            </Grid>
+                            <Grid item xs={6}>
+                              <TextField 
+                                fullWidth 
+                                label="Longueur (cm)" 
+                                margin="normal" 
+                                type="number" 
+                                name="length" 
+                                value={wall.length} 
+                                onChange={(e) => handleWallChange(roomIndex, wallIndex, e)} 
+                              />
+                            </Grid>
+                          </Grid>
+                        ))}
+                        <Button variant="outlined" color="primary" onClick={() => handleAddWall(roomIndex)}>
+                          Ajouter un mur
+                        </Button>
+                        <TextField 
+                          fullWidth 
+                          label="Épaisseur du mur (cm)" 
+                          margin="normal" 
+                          type="number" 
+                          name="thickness" 
+                          value={room.thickness} 
+                          onChange={(e) => handleInputChange(roomIndex, e)} 
+                        />
+                        <TextField 
+                          fullWidth 
+                          label="Nombre de fenêtres" 
+                          margin="normal" 
+                          type="number" 
+                          name="windows" 
+                          value={room.windows} 
+                          onChange={(e) => handleInputChange(roomIndex, e)} 
+                        />
+                        <TextField 
+                          fullWidth 
+                          label="Nombre de portes" 
+                          margin="normal" 
+                          type="number" 
+                          name="doors" 
+                          value={room.doors} 
+                          onChange={(e) => handleInputChange(roomIndex, e)} 
+                        />
+                      </>
+                    )}
+                    {index === 2 && (
+                      <>
+                        <Typography variant="body1" style={{ marginTop: 20 }}>
+                          Téléchargez des images
+                        </Typography>
+                        <Button variant="outlined" component="label" fullWidth style={{ marginTop: 10 }}>
+                          Faites glisser vos fichiers ou parcourez les
+                          <input 
+                            type="file" 
+                            hidden 
+                            multiple 
+                            onChange={(e) => handleFileChange(roomIndex, e)} 
+                          />
+                        </Button>
+                        <Typography variant="body2" style={{ marginTop: 10 }}>
+                          Ne prend en charge que les fichiers .jpg, .png, .svg et les fichiers zip
+                        </Typography>
+                        <Typography variant="body2" style={{ marginTop: 10 }}>
+                          ou
+                        </Typography>
+                        <TextField 
+                          fullWidth 
+                          label="Ajouter un lien URL" 
+                          margin="normal" 
+                          type="url" 
+                        />
+                        <Button variant="contained" color="secondary" style={{ marginTop: 10 }}>
+                          Télécharger
+                        </Button>
+                      </>
+                    )}
+                  </CardContent>
+                </StyledCard>
+              ))}
+              <Box sx={{ mb: 2 }}>
+                <div>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    {index === steps.length - 1 ? 'Terminer' : 'Continuer'}
+                  </Button>
+                  <Button
+                    disabled={index === 0}
+                    onClick={handleBack}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Retour
+                  </Button>
+                </div>
+              </Box>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} sx={{ p: 3 }}>
+          <Typography>Toutes les étapes sont terminées - vous avez fini</Typography>
+          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+            Réinitialiser
+          </Button>
+        </Paper>
+      )}
+      <Box mt={2}>
+        <Button variant="contained" color="primary" onClick={handleAddRoom}>
+          Ajouter une pièce
+        </Button>
+      </Box>
+      <Box mt={2}>
+        <Button variant="contained" color="primary">
+          Enregistrer
+        </Button>
+      </Box>
+      <Box mt={2}>
+        <Button variant="contained" color="secondary">
+          Télécharger en .XLS
+        </Button>
+      </Box>
+    </Content>
+  );
+};
 
-            export default FormulaireCC2;
+export default FormulaireCC2;
+
